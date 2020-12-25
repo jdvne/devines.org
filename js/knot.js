b8586ebf3b96df4d9dad4952cf6e5db862862212
Joshua Devine <joshuaddevine@gmail.com>
2020-12-25 04:57:04 -0500

    pixel ratio

--- js/knot.js
+++ js/knot.js
@@ -8,6 +8,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
 
 container.appendChild( renderer.domElement );
 
