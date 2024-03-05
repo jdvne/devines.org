312af4781d04e5835cdf1f6428ad9485cce26729
Josh Devine <joshuaddevine@gmail.com>
2024-03-05 17:24:21 -0500

    fix: remove trailing spaces

--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -195,37 +195,37 @@ export function Wedding() {
               </>
             }
             role="Bride"
-            bio="Stephanie graduates with her Masters in Public Health with a 
-              concentration in infectious diseases in May 2024. In the little 
-              free time she has, she loves volunteering at the animal shelter, 
+            bio="Stephanie graduates with her Masters in Public Health with a
+              concentration in infectious diseases in May 2024. In the little
+              free time she has, she loves volunteering at the animal shelter,
               taking walks, and going to trivia."
             image={steph}
-            fact="She loves to donate platelets because it helps people 
+            fact="She loves to donate platelets because it helps people
               (nobody talks to her for 3 hours)"
           />
           <Partier
             name="Anouk Rudd"
             role="Maid of Honor"
-            bio="Anouk is from Floyd, Virginia. She has a biology degree from 
-              Longwood University. Her family owns a chocolate shop, Cocoa Mia, 
-              and made the yummy desserts for the reception. Stephanie and 
-              Anouk met in 2018 at Governor’s School in Pulaski, Virginia. 
-              Since then, they have been inseparable and always make time to 
+            bio="Anouk is from Floyd, Virginia. She has a biology degree from
+              Longwood University. Her family owns a chocolate shop, Cocoa Mia,
+              and made the yummy desserts for the reception. Stephanie and
+              Anouk met in 2018 at Governor’s School in Pulaski, Virginia.
+              Since then, they have been inseparable and always make time to
               grab a latte and a not-so-quick shopping trip."
             image={anouk}
-            fact="Anouk enjoys going on the Virginia Department of Health's 
-              website to read the health department inspection records for 
+            fact="Anouk enjoys going on the Virginia Department of Health's
+              website to read the health department inspection records for
               restaurants across the state and judging them."
           />
           <Partier
             name="Kaity McCullough"
             role="Bridesmaid"
-            bio="Kaity is from Powhatan, Virginia, but recently moved to 
-              Colorado Springs, Colorado. She is currently acting as a platoon 
-              leader in the Army at Fort Carson. She loves spending as much 
-              time as possible outside and adventuring around the state. 
-              Stephanie and Kaity met in 2020 in the Virginia Tech Corps of 
-              Cadets. While they only lived together for a year, they were in 
+            bio="Kaity is from Powhatan, Virginia, but recently moved to
+              Colorado Springs, Colorado. She is currently acting as a platoon
+              leader in the Army at Fort Carson. She loves spending as much
+              time as possible outside and adventuring around the state.
+              Stephanie and Kaity met in 2020 in the Virginia Tech Corps of
+              Cadets. While they only lived together for a year, they were in
               each other’s rooms enough that you would never know."
             image={kaity}
             fact="Kaity doesn't have a fun fact. :("
@@ -233,23 +233,23 @@ export function Wedding() {
           <Partier
             name="Julia Moschella"
             role="Bridesmaid"
-            bio="Julia is from Christiansburg, Virginia, but currently lives in 
-              Charlottesville, Virginia. She is a 2024 graduate with a Master's 
-              in exercise physiology and is planning to start working full time 
-              at a cardiac rehab later this summer. She loves to do just about 
-              anything outdoors, exploring new places, and hanging out with 
-              friends. Stephanie and Julia met in middle school through band, 
+            bio="Julia is from Christiansburg, Virginia, but currently lives in
+              Charlottesville, Virginia. She is a 2024 graduate with a Master's
+              in exercise physiology and is planning to start working full time
+              at a cardiac rehab later this summer. She loves to do just about
+              anything outdoors, exploring new places, and hanging out with
+              friends. Stephanie and Julia met in middle school through band,
               soccer, basketball, and probably anything else."
             image={julia}
-            fact="In middle school, Julia's nickname was Pancakes because she 
+            fact="In middle school, Julia's nickname was Pancakes because she
               was a 'short stack'."
           />
           <Partier
             name="Lindsey Devine"
             role="Bridesmaid"
-            bio="Lindsey is from Morganton, North Carolina. She is the second 
-              youngest of the Devine siblings and the only girl. She graduates 
-              from Patton High School in June 2024 and will be attending 
+            bio="Lindsey is from Morganton, North Carolina. She is the second
+              youngest of the Devine siblings and the only girl. She graduates
+              from Patton High School in June 2024 and will be attending
               college in the fall."
             image={lindsey}
             fact="Lindsey is left-handed and enjoys Frank Ocean."
@@ -257,10 +257,10 @@ export function Wedding() {
           <Partier
             name="Joshua Devine"
             role="Groom"
-            bio="Joshua graduated with his B.S. in Computer Science from the 
-              University of Virginia in May 2022. Since then, he has been a 
-              Software Engineer at Capital One. In his free time, you'll 
-              probably find him on a hiking trail or a disc golf course 
+            bio="Joshua graduated with his B.S. in Computer Science from the
+              University of Virginia in May 2022. Since then, he has been a
+              Software Engineer at Capital One. In his free time, you'll
+              probably find him on a hiking trail or a disc golf course
               somewhere."
             image={josh}
             fact="Joshua would happily pick up any frog he comes across."
@@ -269,12 +269,12 @@ export function Wedding() {
           <Partier
             name="Ian Switzer"
             role="Best Man"
-            bio="Ian is from Richmond, Virginia, but currently lives in 
-              Raleigh, North Carolina with his wife Hannah. Ian graduated from 
-              the University of Virginia with a B.S. in Computer Science in 
-              2023; he is now a Software Engineer at EngageSmart. Since moving 
-              into his new home, he has been 3D printing anything and 
-              everything. Joshua and Ian met at UVA through the 'CS Pilot 
+            bio="Ian is from Richmond, Virginia, but currently lives in
+              Raleigh, North Carolina with his wife Hannah. Ian graduated from
+              the University of Virginia with a B.S. in Computer Science in
+              2023; he is now a Software Engineer at EngageSmart. Since moving
+              into his new home, he has been 3D printing anything and
+              everything. Joshua and Ian met at UVA through the 'CS Pilot
               Program' which definitely wasn't about planes."
             image={ian}
             fact="Ian has been skydiving both indoors and outdoors."
@@ -283,13 +283,13 @@ export function Wedding() {
           <Partier
             name="Jared Arnold"
             role="Groomsman"
-            bio="Jarold is from Wytheville, Virginia, but now resides in 
-              Williamsburg, Virginia. Jared also graduated from the University 
-              of Virginia with a B.S. in Computer Science in 2023. He is now an 
-              App Engineer at Index AR Solutions. Jared is an explorer at 
-              heart--he's roadtripped across the US and recently ventured 
-              across Iceland in a van. Joshua and Jared have known eachother 
-              for too long. They met in kindergarten, grew up together in 
+            bio="Jarold is from Wytheville, Virginia, but now resides in
+              Williamsburg, Virginia. Jared also graduated from the University
+              of Virginia with a B.S. in Computer Science in 2023. He is now an
+              App Engineer at Index AR Solutions. Jared is an explorer at
+              heart--he's roadtripped across the US and recently ventured
+              across Iceland in a van. Joshua and Jared have known eachother
+              for too long. They met in kindergarten, grew up together in
               Wytheville, and were college roommates at UVA."
             image={jared}
             fact="Jared saved Joshua's life once."
@@ -298,25 +298,25 @@ export function Wedding() {
           <Partier
             name="Joseph Banks"
             role="Groomsman"
-            bio="Joseph, big surprise, also graduated from the University of 
-              Virginia with a B.S. in Computer Science in 2022. He is now a 
-              Software Engineer at DataDog, working from the New York Times 
-              Building in NYC. Joshua and Joseph may be thousands of miles 
-              apart, but the moment they reconvene, they begin scheming like 
+            bio="Joseph, big surprise, also graduated from the University of
+              Virginia with a B.S. in Computer Science in 2022. He is now a
+              Software Engineer at DataDog, working from the New York Times
+              Building in NYC. Joshua and Joseph may be thousands of miles
+              apart, but the moment they reconvene, they begin scheming like
               they always do. Just ask Ian."
             image={joseph}
-            fact="Joseph was born in a log cabin in the middle of Vermont 
+            fact="Joseph was born in a log cabin in the middle of Vermont
               during a snowstorm."
             right
           />
           <Partier
             name="Noah Devine"
             role="Groomsman"
-            bio="Noah is the second oldest of the Devine siblings. He is 
-              currently attending Virginia Commonwealth University and will 
-              graduate in May 2024 with a B.S. in Financial Technology with a 
-              concentration in Actuarial Science. In a unique turn of events, 
-              Joshua and Noah have had the privilege to live together in 
+            bio="Noah is the second oldest of the Devine siblings. He is
+              currently attending Virginia Commonwealth University and will
+              graduate in May 2024 with a B.S. in Financial Technology with a
+              concentration in Actuarial Science. In a unique turn of events,
+              Joshua and Noah have had the privilege to live together in
               Richmond."
             image={noah}
             fact="Noah is an amateur filmmaker."
