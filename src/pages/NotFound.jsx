a164033ca5c702d94f03989dd1889e742e873f44
Joshua Devine <joshuaddevine@gmail.com>
2025-06-09 22:38:10 -0400

    fix: 404 breadcrumb

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import { Breadcrumb } from "../components/Breadcrumb";
 
 import styles from './Home.module.css';
 
@@ -11,14 +11,8 @@ export function NotFound() {
       </Helmet>
       <div className={styles.container}>
         <h1>404 Not Found</h1>
-        <p>Sorry, the page you are looking for doesn't exist.</p>
-        <div>
-          <nav className={styles.nav}>
-            <Link to="/" className={styles.navLink}>
-              <span>{`<home>`}</span>
-            </Link>
-          </nav>
-        </div>
+        <p>Sorry, the page you are looking for doesn&#39;t exist.</p>
+        <Breadcrumb />
       </div>
     </main>
   );
