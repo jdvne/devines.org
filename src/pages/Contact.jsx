e8590332ff2d7eb90725649abc18c6b3a0a670bc
Joshua Devine <joshuaddevine@gmail.com>
2025-06-14 13:01:18 -0400

    feat: better style on contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,16 +4,19 @@ import { Breadcrumb } from '../components/Breadcrumb';
 const lt = '<';
 const gt = '>';
 
+const github = 'const GitHub = () => '
+const linkedin = 'const LinkedIn = () => ';
+
 function Contact() {
   return (
     <div className={styles.thought}>
       <Breadcrumb crumbs={[{ label: 'Home', path: '/' }, { label: 'Contact' }]} />
       <h1>Contact</h1>
       <p>
-        GitHub: <a href="https://github.com/jdvne"><span className={styles.tag}>{lt}</span>github.com/joshua-devine<span className={styles.tag}>{gt}</span></a>
+        {github}<a href="https://github.com/jdvne"><span className={styles.tag}>{lt}a href=</span>github.com/joshua-devine<span className={styles.tag}>/{gt}</span></a>
       </p>
       <p>
-        LinkedIn: <a href="https://www.linkedin.com/in/jdvne"><span className={styles.tag}>{lt}</span>linkedin.com/in/joshua-devine<span className={styles.tag}>{gt}</span></a>
+        {linkedin}<a href="https://www.linkedin.com/in/jdvne"><span className={styles.tag}>{lt}a href=</span>linkedin.com/in/joshua-devine<span className={styles.tag}>/{gt}</span></a>
       </p>
     </div>
   );
