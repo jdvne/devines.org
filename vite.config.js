995e687df8dabcdaa7b25d0a46fe081d7b81838b
Joshua Devine <joshuaddevine@gmail.com>
2025-06-14 13:02:48 -0400

    fix: test did not work

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/devines.org/",
+  base: "/",
   assetsInclude: ['**/*.md'],
   define: {
     global: {},
